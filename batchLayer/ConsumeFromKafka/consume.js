4b6cf754be5318805de0b7970d57e3a6d00ffe75
avivdan <aviv.danino@boyar.org.il>
2023-03-12 11:51:57 +0200

    change gitignore

--- batchLayer/ConsumeFromKafka/consume.js
+++ batchLayer/ConsumeFromKafka/consume.js
@@ -1,52 +1,33 @@
-// https://www.cloudkarafka.com/ הפעלת קפקא במסגרת ספק זה
+const { KafkaConsumer } = require("node-rdkafka");
 
-const uuid = require("uuid");
-const Kafka = require("node-rdkafka");
 
-const kafkaConf = {
+const consumer = new KafkaConsumer({
   "group.id": "cloudkarafka-example",
-  "bootstrap.servers":"pkc-129zv.me-south-1.aws.confluent.cloud:9092",
+  "bootstrap.servers": "pkc-129zv.me-south-1.aws.confluent.cloud:9092",
   "socket.keepalive.enable": true,
   "security.protocol": "SASL_SSL",
   "sasl.mechanisms": "PLAIN",
   "sasl.username": "5NQIKJ3QRGYLWSR4",
   "sasl.password": "m1LTLAMxlvowugr5Vp9/Zo5zEugypK6RqM3DaoS5YDNOWvw++8XCAUAgGSGkajXF",
   "debug": "generic,broker,security"
-};
+});
+const topic = "topic_0";
 
-// const prefix = "mo0oa5gi-";
-const topic = `topic_0`;
-const producer = new Kafka.Producer(kafkaConf);
-
-const genMessage = m => new Buffer.alloc(m.length,m);
-//const prefix = process.env.CLOUDKARAFKA_USERNAME;
-
-const topics = [topic];
-const consumer = new Kafka.KafkaConsumer(kafkaConf, {
-  "auto.offset.reset": "beginning"
-});
-
-consumer.on("error", function(err) {
-  console.error(err);
-});
-consumer.on("ready", function(arg) {
-  console.log(`Consumer ${arg.name} ready`);
-
-  consumer.subscribe(topics);
-  consumer.consume();
-});
-
-consumer.on("data", function(m) {
- console.log(m.value.toString());
-});
-consumer.on("disconnected", function(arg) {
-  process.exit();
-});
-consumer.on('event.error', function(err) {
-  console.error(err);
-  process.exit(1);
-});
-consumer.on('event.log', function(log) {
-  console.log(log);
-});
 consumer.connect();
+consumer
+  .on("ready", (arg) => {
+      consumer.subscribe([topic]).consume();
+      console.log(`Consumer ${arg.name} ready. topics: ${topic}`);
+  })
+  .on("disconnected", (arg) =>
+      console.log(`Consumer ${arg.name} disconnected.`)
+  )
+  .on("event.error", (err) => {
+      console.error(err);
+      process.exit(1);
+  });
+  consumer.on("data", function (msg) {
+      console.log(msg.value.toString());
+  });
+
+module.exports = consumer;
